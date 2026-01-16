dc839f6c64f344e3d02d019110b0a959b8f273df
Alex Scott <codetheorist@outlook.com>
2026-01-16 22:27:57 +0000

    feat: create required server endpoints

---
+++ server/middleware/auth.ts
@@ -0,0 +1,34 @@
+export default defineEventHandler(async (event) => {
+  const session = await getUserSession(event)
+  event.context.canUnlockNuxterBadge = false
+  event.context.canUnlockSoundsterBadge = false
+
+  if (session.data.githubId) {
+    const [
+      contributors,
+      // moduleMaintainers,
+      nuxtUIProOutsideCollaborators
+    ] = await Promise.all([
+      fetchContributors(event),
+      // fetchModuleMaintainers(),
+      fetchNuxtUIProOutsideCollaborators(),
+    ])
+
+    const contributor = contributors?.find(contributor => contributor.githubId === String(session.data.githubId)) || {
+      githubId: session.data.githubId,
+      username: session.data.githubUsername,
+      issues: 0,
+      merged_pull_requests: 0,
+      helpful_issues: 0,
+      comments: 0,
+      helpful_comments: 0,
+      reactions: 0,
+      score: 0,
+    }
+    event.context.contributor = contributor
+    // event.context.canUnlockModuleBadge = !!(moduleMaintainers?.find(maintainer => maintainer.github?.toLowerCase() === String(session.data.githubUsername).toLowerCase()))
+    event.context.canUnlockNuxterBadge = (contributor.helpful_comments + contributor.helpful_issues + contributor.merged_pull_requests) > 0
+    event.context.canUnlockSoundsterBadge = (contributor.helpful_comments + contributor.helpful_issues + contributor.merged_pull_requests) > 0
+    event.context.canUnlockUIProBadge = event.context.canUnlockUIProBadge || nuxtUIProOutsideCollaborators?.includes(session.data.githubUsername)
+  }
+})
