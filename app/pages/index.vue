a71d6f1cba079738bdcadd91d08b96647865d1e6
Alex Scott <codetheorist@outlook.com>
2026-01-16 22:28:30 +0000

    feat: create app layout and pages

---
+++ app/pages/index.vue
@@ -0,0 +1,24 @@
+<script setup>
+useHead({
+  link: [{ rel: 'canonical', href: 'https://soundsters.soundsdesigned.com' }],
+})
+
+useSeoMeta({
+  title: 'Are you a Soundster?',
+  ogTitle: 'Are you a Soundster?',
+  description: 'Discover your contributions to unlock the soundster role on Sounds Designed Discord server.',
+  ogDescription: 'Discover your contributions to unlock the soundster role on Sounds Designed Discord server.',
+  // ogImage: 'https://soundsters.soundsdesigned.com/social-card.jpg',
+  ogImageAlt: 'Soundsters',
+  // twitterImage: 'https://soundsters.soundsdesigned.com/social-card.jpg',
+  ogUrl: 'https://soundsters.soundsdesigned.com',
+})
+</script>
+
+<template>
+  <div class="text-green-400 flex flex-col gap-y-[72px] py-10 lg:py-40">
+    <HomeHero />
+    <HomeContributors />
+    <HomeJoin />
+  </div>
+</template>
