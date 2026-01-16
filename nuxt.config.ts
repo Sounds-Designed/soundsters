6a6f653670f7e41a7eaf56382afd371162ce8b04
Alex Scott <codetheorist@outlook.com>
2026-01-16 22:16:40 +0000

    feat: create Nuxt config files
    * Create `.editorconfig`
    * Create ES Lint config
    * Create Nuxt config
    * Create NPM package manifest
    * Add PNPM lockfile
    * Renovate config
    * TS config
    * Vitest config

---
+++ nuxt.config.ts
@@ -0,0 +1,88 @@
+import { defineNuxtConfig } from 'nuxt/config'
+
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/ui',
+    '@nuxtjs/plausible',
+    '@vueuse/nuxt',
+    'nuxt-og-image',
+    '@nuxt/image',
+    '@nuxt/test-utils/module',
+    '@nuxthub/core',
+  ],
+  devtools: { enabled: true },
+
+  app: {
+    head: {
+      htmlAttrs: { lang: 'en' },
+    },
+    pageTransition: false,
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  runtimeConfig: {
+    sessionPassword: '',
+    github: {
+      accessToken: '',
+      clientId: '',
+      clientSecret: '',
+    },
+    discord: {
+      clientId: '',
+      clientSecret: '',
+      guildId: '',
+      nuxterRoleId: '',
+      soundsterRoleId: '',
+      moduleMaintainerRoleId: '',
+      uIProRoleId: '',
+      botToken: '',
+    },
+  },
+
+  routeRules: {
+    '/card/**': { proxy: '/__og-image__/image/**' },
+  },
+
+  experimental: {
+    viewTransition: true,
+  },
+
+  compatibilityDate: '2025-07-31',
+
+  hub: {
+    cache: true,
+  },
+
+  typescript: {
+    tsConfig: {
+      include: ['../test'],
+    },
+    nodeTsConfig: {
+      include: ['../vitest.config.ts'],
+    },
+  },
+
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+
+  image: {
+    provider: 'github',
+  },
+
+  ogImage: {
+    runtimeCacheStorage: {
+      driver: 'vercel-runtime-cache',
+    },
+    compatibility: {
+      runtime: {
+        chromium: false,
+      },
+    },
+  },
+})
