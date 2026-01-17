53f7a1d5864aa9591f464953864ca0ba37a4911f
Alex Scott <codetheorist@outlook.com>
2026-01-17 00:47:02 +0000

    feat: add missing components

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+import type { DropdownMenuItem } from '@nuxt/ui'
+
+const links: DropdownMenuItem[] = [
+  {
+    label: 'UI Docs',
+    description: 'UI component documentation',
+    to: 'https://ui.soundsdesigned.com/?utm_source=soundsters&utm_medium=dropdown&utm_campaign=home',
+    icon: 'i-simple-icons-nuxtdotjs',
+    target: '_blank',
+  },
+  {
+    label: 'Nuxt.com',
+    description: 'Nuxt documentation',
+    to: 'https://nuxt.com/?utm_source=soundsters&utm_medium=dropdown&utm_campaign=home',
+    icon: 'i-simple-icons-nuxtdotjs',
+    target: '_blank',
+  },
+  {
+    label: 'Nuxt.new',
+    description: 'Nuxt templates',
+    to: 'https://nuxt.new/?utm_source=soundsters&utm_medium=dropdown&utm_campaign=home',
+    icon: 'i-simple-icons-nuxtdotjs',
+    target: '_blank',
+  },
+]
+</script>
+
+<template>
+  <div class="h-20 w-full sticky top-0 bg-neutral-950/70 backdrop-blur-md z-50 flex flex-col justify-center">
+    <UContainer class="flex justify-between items-center w-full">
+      <div class="flex items-center">
+        <NuxtLink
+          to="/"
+          aria-label="go to home page"
+          class="flex items-center"
+        >
+          <TheLogo class="h-6" />
+        </NuxtLink>
+        <UDropdownMenu
+          :items="links"
+          size="xl"
+        >
+          <UButton
+            variant="ghost"
+            color="neutral"
+            icon="i-ph-caret-down-fill"
+            square
+            class="ml-1 opacity-50 hover:opacity-100"
+            aria-label="dropdown button"
+          />
+        </UDropdownMenu>
+      </div>
+      <div class="flex items-center sm:gap-x-4 text-white font-medium">
+        <UButton
+          class="flex items-center sm:-mr-2"
+          to="https://go.nuxt.com/discord"
+          target="_blank"
+          variant="link"
+          color="neutral"
+          size="xl"
+          aria-label="join us"
+        >
+          <UIcon
+            name="i-simple-icons-discord"
+            class="h-5 w-5"
+          />
+          <span class="hidden sm:block">Join us</span>
+        </UButton>
+        <div class="hidden sm:block h-8 w-px bg-neutral-700 -mr-2 sm:-mr-6" />
+        <UButton
+          to="http://go.nuxt.com/github"
+          class="flex gap-x-2 items-center"
+          variant="link"
+          color="neutral"
+          size="xl"
+          target="_blank"
+          aria-label="star us"
+        >
+          <UIcon
+            name="i-simple-icons-github"
+            class="h-5 w-5 sm:ml-4"
+          />
+          <span class="hidden sm:block">Star us</span>
+        </UButton>
+      </div>
+    </UContainer>
+  </div>
+</template>
